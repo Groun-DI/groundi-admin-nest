8b842853689c6d685c3dbc0814975385ccb44ffb
lgu0521 <lgu0521@gmail.com>
2022-04-23 13:20:51 +0900

    studio controller -> api url setting

--- src/api/studio/studio.controller.ts
+++ src/api/studio/studio.controller.ts
@@ -1,5 +1,5 @@
 
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, UseGuards, Put } from '@nestjs/common';
 import { StudioService } from './studio.service';
 import { JwtAuthGuard } from 'src/auth-guard/jwt/jwt.auth-guard';
 import { Studio as StudioModel } from '@prisma/client';
@@ -11,60 +11,55 @@ import { ComplimentaryUpdateBody } from 'src/dto/complimentary-update';
 import { PrecautionUpdateBody } from 'src/dto/precaution-update.body';
 
 
-@Controller('studio')
+@Controller('center/:centerId/studio')
 
 export class StudioController {
   constructor(private readonly studioService: StudioService) { }
 
-  @Post(':centerId')
+  @Post()
   studioCreate(@Param('centerId') centerId: number, @Body() body: StudioCreateBody) {
     return this.studioService.studioCreate(centerId, body);
   }
 
-  // @Post(':studioId/image')
-  // studioImageCreate(@Param('studioId') studioId: number, @Body() body: StudioImageCreateBody) {
-  //   return this.studioService.studioImageCreate(+studioId, body);
-  // }
+  @Post(':studioId/image')
+  studioImageCreate(@Param('studioId') studioId: number, @Body() body: StudioImageCreateBody) {
+    return this.studioService.studioImageCreate(+studioId, body);
+  }
 
-  @Patch(':studioId')
+  @Put(':studioId')
   studioUpdate(@Param('studioId') studioId: number, @Body() body: StudioUpdateBody) {
     return this.studioService.studioUpdate(+studioId, body);
   }
 
-  @Patch(':studioId/amenity')
+  @Put(':studioId/amenity')
   amenityUpdate(@Param('studioId') studioId: number, @Body() body: AmenityUpdateBody[]) {
     return this.studioService.amenityUpdate(+studioId, body);
   }
 
-  @Patch(':studioId/complimentary')
+  @Put(':studioId/complimentary')
   complimentaryUpdate(@Param('studioId') studioId: number, @Body() body: ComplimentaryUpdateBody[]) {
     return this.studioService.complimentaryUpdate(+studioId, body);
   }
 
-  @Patch(':studioId/precaution')
+  @Put(':studioId/precaution')
   precautionUpdate(@Param('studioId') studioId: number, @Body() body: PrecautionUpdateBody[]) {
     return this.studioService.precautionUpdate(+studioId, body);
   }
 
-  // @Patch(':studioId/image')
-  // studioImageUpdate(@Param('studioId') studioId: number, @Body() body: StudioImageCreateBody){
-  //   return this.studioService.studioImageUpdate(+studioId, body);
-  // }
-
-  // @Delete(':studioId')
-  // studioDelete(@Param('studioId') studioId: number) {
-  //   return this.studioService.studioDelete(+studioId);
-  // }
-
-  @Delete('image/:imageId')
-  studioImageDelete(@Param('imageId') imageId: number) {
-    return this.studioService.studioImageDelete(+imageId);
+  @Put(':studioId/image')
+  studioImageUpdate(@Param('studioId') studioId: number, @Body() body: StudioImageCreateBody) {
+    return this.studioService.studioImageUpdate(+studioId, body);
   }
 
-  // @Get(':centerId')
-  // studioFindAll(@Param('centerId') centerId: number) {
-  //   return this.studioService.studioFindAll(+centerId);
-  // }
+  @Delete(':studioId')
+  studioDelete(@Param('studioId') studioId: number) {
+    return this.studioService.studioDelete(+studioId);
+  }
+
+  @Get()
+  studioFindAll(@Param('centerId') centerId: number) {
+    return this.studioService.studioFindAll(+centerId);
+  }
 
   @Get(':studioId')
   studioFindOne(@Param('studioId') studioId: string): Promise<StudioModel> {
