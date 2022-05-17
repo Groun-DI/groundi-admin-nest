083ed602c06914b87cc7f50fa72cadbdea27f259
lgu0521 <lgu0521@gmail.com>
2022-05-17 21:54:52 +0900

    make it better

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,9 @@ async function bootstrap() {
     .enableCors();
   app.setGlobalPrefix('/api');
 
-  await app.listen(port, host);
+  await app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode");
+  });
 }
 
 
